2a0f82d5755c1c6f307618eab2bad2cff8475342
Taaniel Kõmmus <TaanielKommus@users.noreply.github.com>
2017-04-07 10:28:07 +0300

    10

--- src/main/java/taankomm/Aplikatsioon.java
+++ src/main/java/taankomm/Aplikatsioon.java
@@ -1,5 +1,5 @@
 package taankomm;
-
+import java.io.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
