a7642593148c34bed7ddc9dd460495d33247c2d9
Taaniel Kõmmus <TaanielKommus@users.noreply.github.com>
2017-04-07 10:04:05 +0300

    6

--- src/main/java/taankomm/Keskminekiirus.java
+++ src/main/java/taankomm/Keskminekiirus.java
@@ -1,13 +1,13 @@
 package taankomm;
 
-public class Keskminekiirus implements Funktsioon{
+public class Keskminekiirus implements Liides{
 	double v;
 	public String f(double s, double h, double m){
 		double t = h + (m/60);
 
 		v = Math.round(s/t);
 
-			return "Keskmine kiirus oli "+ v +" km/h";
+			return "Keskmine kiirus oli "+ v +" km/h.";
 
 	}
 
